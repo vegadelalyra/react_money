be88e20bff885f902060b5a0d802aa2168145191
Vegadelalyra <lyradeperseo@gmail.com>
2024-01-16 19:38:56 -0500

    chore: start vite react js project + implement firebase services with github actions + docs. Next step: implement Redux

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyD_1tl8BAcQ5jfhlVIxHARpjCdV2gKZ0aE",
+  authDomain: "makaia-react-money.firebaseapp.com",
+  projectId: "makaia-react-money",
+  storageBucket: "makaia-react-money.appspot.com",
+  messagingSenderId: "860516876237",
+  appId: "1:860516876237:web:460d3c50c88a9e28da8e84"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
