5ac2b2c75de727aeca2d4afb9b40d0be11be5f10
Vegadelalyra <lyradeperseo@gmail.com>
2024-01-22 23:12:23 -0500

    feat: login page view

--- src/pages/LoginPage/index.jsx
+++ src/pages/LoginPage/index.jsx
@@ -1,21 +1,19 @@
 // LoginPage.js
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
+import React from 'react';
 import { auth, googleAuthProvider } from '../../firebase';
 import { signInWithPopup } from 'firebase/auth';
+import './styles.sass'
+
+import hero_mobile_svg from '../../assets/login/hero_mobile.svg';
+import makaiapp_logo_svg from '../../assets/login/makaiapp_logo.svg';
+import google_icon_svg from '../../assets/login/google_icon.svg'
 
 const LoginPage = () => {
-    const dispatch = useDispatch();
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
-
-    const handleEmailChange = e => setEmail(e.target.value);
-    const handlePasswordChange = e => setPassword(e.target.value);
-
     const handleGoogleLogin = async () => {
         try {
-            await signInWithPopup(auth, googleAuthProvider);
+            const oAuth2 = await signInWithPopup(auth, googleAuthProvider);
             // Dispatch an action if needed
+            console.log(oAuth2)
         } catch (error) {
             console.error('Error logging in with Google:', error.message);
         }
@@ -23,11 +21,19 @@ const LoginPage = () => {
 
     return (
         <div>
-            <h2>Login</h2>
             <picture>
-                <img src='../../assets/hero_mobile.svg' />
+                <img
+                    src={makaiapp_logo_svg}
+                    alt='Here shall be our logo'
+                />
+                <img
+                    src={hero_mobile_svg}
+                    alt='Welcome to Makaiapp!'
+                />
             </picture>
-            <button onClick={handleGoogleLogin}>Login with Google</button>
+            <button onClick={handleGoogleLogin}>
+                <img src={google_icon_svg} id='googleG' /> Log in with Google
+            </button>
         </div>
     );
 };
